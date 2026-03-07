739dc469857d96e00ee01421ed2daa404a1f217b
Mohammad Ashkani <64630663+mashkani@users.noreply.github.com>
2026-03-06 19:04:28 -0800

    added description & connect section to footer

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -96,23 +96,45 @@ const config: Config = {
     },
     footer: {
       style: 'light',
-      logo: {
-        alt: 'Trilemma Foundation',
-        src: 'img/trilemma_foundation.png',
-        srcDark: 'img/trilemma_foundation_white.png',
-        href: '/',
-        height: 26,
-      },
       links: [
         {
+          className: 'footer-col--foundation',
           items: [
             {
-              label: 'GitHub',
-              href: REPO_URL,
+              html: `<div class="footer-foundation">
+  <a class="footer-foundation__logo-link" href="/" aria-label="Trilemma Foundation home">
+    <img class="footer-foundation__logo" src="/img/trilemma_foundation_white.png" alt="Trilemma Foundation" loading="lazy" />
+  </a>
+  <p class="footer-foundation__description">
+    Trilemma Foundation is a Canadian Registered Charity that incubates technical talent through global university partnerships, open source collaboration, and performance based opportunities. Our mission is to enable the brightest minds to rise based on performance.
+  </p>
+</div>`,
             },
+          ],
+        },
+        {
+          title: 'Connect',
+          className: 'footer-col--connect',
+          items: [
             {
               label: 'Discord',
               href: 'https://discord.gg/AS7WMx7Cy2',
+            },
+            {
+              label: 'X (Twitter)',
+              href: 'https://x.com/TrilemmaFdn',
+            },
+            {
+              label: 'LinkedIn',
+              href: 'https://www.linkedin.com/company/trilemma-foundation/',
+            },
+            {
+              label: 'GitHub',
+              href: 'https://github.com/TrilemmaFoundation',
+            },
+            {
+              label: 'Email',
+              href: 'mailto:matt@trilemma.foundation',
             },
           ],
         },
